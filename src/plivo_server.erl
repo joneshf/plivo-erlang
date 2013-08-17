cbd770a4132d03fe9beb07777dd9671d38edea61
joneshf <jones3.hardy@gmail.com>
2013-08-17 16:50:31 -0700

    Cleanup.

--- src/plivo_server.erl
+++ src/plivo_server.erl
@@ -2,21 +2,21 @@
 
 -behaviour(gen_server).
 
--export([start_link/0, get_account/1]).
+%% Plivo Api
+-export([get_account/1]).
 
+%% gen_server stuff
+-export([start_link/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--define (ACCOUNT_URI, "https://api.plivo.com/v1/Account/").
-
--define (AUTH_HEADER,
-         {"Authorization",
-          "Basic " ++ base64:encode_to_string(?AUTH_ID ++ ":" ++ ?AUTH_TOKEN)}).
-
--define (AUTH_ID, "").
-
--define (AUTH_TOKEN, "").
-
+%% Plivo Account info.
+-define(ACCOUNT_URI, "https://api.plivo.com/v1/Account/").
+-define(AUTH_HEADER,
+        {"Authorization",
+         "Basic " ++ base64:encode_to_string(?AUTH_ID ++ ":" ++ ?AUTH_TOKEN)}).
+-define(AUTH_ID, "").
+-define(AUTH_TOKEN, "").
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
@@ -26,37 +26,36 @@ init([]) ->
     ssl:start(),
     {ok, []}.
 
-%% callbacks
+%% Gen_server callbacks.
 
 handle_call({get, Payload}, _From, State) ->
     {ok, Response} = request(get, Payload),
-    {{_Protocol, _StatusCode, _Reason}, _Headers, Body} = Response,
-    {struct, Data} = mochijson:decode(Body),
-    {reply, Data, State};
-handle_call(_Request, _From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
+    Data = process_response(Response),
+    {reply, Data, State}.
 
-handle_cast(_Msg, State) ->
-    {noreply, State}.
+handle_cast(_Msg, State) -> {noreply, State}.
 
-handle_info(_Info, State) ->
-    {noreply, State}.
+handle_info(_Info, State) -> {noreply, State}.
 
-terminate(_Reason, _State) ->
-    ok.
+terminate(_Reason, _State) -> ok.
 
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
 
 %% Helpers.
-request(Method, Payload) ->
-    case Method of
-        get -> httpc:request(get, Payload, [], [])
-    end.
+parse_json(Json) ->
+    {struct, Data} = mochijson:decode(Body),
+    Data.
 
-rest_api(get, Uri) ->
-    gen_server:call(?MODULE, {get, {Uri, [?AUTH_HEADER]}}).
+%% TODO: Handle individual responses.
+process_response({{_,Code,_R},_H,Body}) when Code =< 199 -> Body;
+process_response({{_,Code,_R},_H,Body}) when Code =< 299 -> parse_json(Body);
+process_response({{_,Code,_R},_H,Body}) when Code =< 399 -> Body;
+process_response({{_,Code,_R},_H,Body}) when Code =< 499 -> Body;
+process_response({{_,Code,_R},_H,Body}) when Code =< 599 -> Body.
+
+request(get, Payload) -> httpc:request(get, Payload, [], []).
+
+rest_api(get, Uri) -> gen_server:call(?MODULE, {get, {Uri, [?AUTH_HEADER]}}).
 
 %% Api.
 get_account(AuthID) -> rest_api(get, ?ACCOUNT_URI ++ AuthID).
