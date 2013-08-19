6c782e6b204d283f60242093b8b423b8ebbe6a67
joneshf <jones3.hardy@gmail.com>
2013-08-19 15:12:00 -0700

    Added application api.

--- src/rest_api.erl
+++ src/rest_api.erl
@@ -6,10 +6,15 @@
 -export ([set_auth_id/1, set_auth_token/1]).
 
 %% Plivo Api
+
 %% Account.
 -export([create_subaccount/1, delete_subaccount/1, get_account/0,
-         get_all_subaccounts/0, get_all_subaccounts/1, get_subaccount/1,
+         get_subaccounts/0, get_subaccounts/1, get_subaccount/1,
          modify_account/1, modify_subaccount/2]).
+
+%% Application.
+-export([create_application/1, delete_application/1, get_application/1,
+         get_applications/0, get_applications/1, modify_application/2]).
 
 %% gen_server stuff
 -export([start_link/0]).
@@ -161,41 +166,37 @@ set_auth_token(Token) -> gen_server:cast(?MODULE, {auth_token, Token}).
 %%      name is the name of the subaccount.
 %%      enabled specifies whether a subaccount should be enabled.
 -spec create_subaccount(Params::params()) -> json_term().
-create_subaccount(Params) ->
-    api(post, "Subaccount/", Params).
+create_subaccount(Params) -> api(post, "Subaccount/", Params).
 
 %% @spec delete_subaccount(SId::string()) -> json_term()
 %% @doc Removes the subaccount from the specified account.
 -spec delete_subaccount(SId::string()) -> json_term().
-delete_subaccount(SId) ->
-    api(delete, "Subaccount/" ++ SId ++ "/").
+delete_subaccount(SId) -> api(delete, "Subaccount/" ++ SId ++ "/").
 
 %% @spec get_account() -> json_term()
 %% @doc Returns the account information for the supplied AId.
 -spec get_account() -> json_term().
 get_account() -> api(get, "").
 
-%% @spec get_all_subaccounts() -> json_term()
+%% @spec get_subaccounts() -> json_term()
 %% @doc Returns the subaccounts information for the supplied AId.
--spec get_all_subaccounts() -> json_term().
-get_all_subaccounts() -> get_all_subaccounts([]).
+-spec get_subaccounts() -> json_term().
+get_subaccounts() -> get_subaccounts([]).
 
-%% @spec get_all_subaccounts(Params::params()) -> json_term()
+%% @spec get_subaccounts(Params::params()) -> json_term()
 %% @doc Returns the subaccounts information for the supplied AId.
 %%      Optional params are: limit, offset.
 %%      limit is the maximum number of results returned.  Max 20.
 %%      offset is the subaccount start number.  Zero based.
 %%      That is, if you want accounts 23-29, you would pass in params
 %%      [{limit, 7}, {offset, 22}]
--spec get_all_subaccounts(Params::params()) -> json_term().
-get_all_subaccounts(Params) ->
-    api(get, "Subaccount/", Params).
+-spec get_subaccounts(Params::params()) -> json_term().
+get_subaccounts(Params) -> api(get, "Subaccount/", Params).
 
 %% @spec get_subaccount(SId::string()) -> json_term()
 %% @doc Returns the subaccount information for the supplied SId combo.
 -spec get_subaccount(SId::string()) -> json_term().
-get_subaccount(SId) ->
-    api(get, "Subaccount/" ++ SId ++ "/").
+get_subaccount(SId) -> api(get, "Subaccount/" ++ SId ++ "/").
 
 %% @spec modify_account(Params::params()) -> json_term()
 %% @doc Modifies an existing account.
@@ -216,4 +217,99 @@ modify_account(Params) -> api(post, "", Params).
 modify_subaccount(SId, Params) ->
     api(post, "Subaccount/" ++ SId ++ "/", Params).
 
-%% @spec create_application()
+%% @spec create_application(Params::params()) -> json_term()
+%% @doc Creates a new application.
+%%      Required params:
+%%          answer_url  The URL Plivo will fetch when a call executes this
+%%                      application.
+%%          app_name    The name of your application.
+%%      Optional params:
+%%          answer_method        The method used to call the answer_url.
+%%                               Defaults to POST.
+%%          hangup_url           The URL that will be notified by Plivo
+%%                               when the call hangs up.
+%%          hangup_method        The method used to call the hangup_url.
+%%                               Defaults to POST.
+%%          fallback_answer_url  Invoked by Plivo only if answer_url is
+%%                               unavailable or the XML response is invalid.
+%%                               Should contain a XML response.
+%%          fallback_method      The method used to call the
+%%                               fallback_answer_url.
+%%                               Defaults to POST.
+%%          message_url          The URL that will be notified by Plivo
+%%                               when an inbound message is received.
+%%                               Defaults not set.
+%%          message_method       The method used to call the message_url.
+%%                               Defaults to POST.
+%%          default_number_app   If set to true,
+%%                               this parameter ensures that
+%%                               newly created numbers,
+%%                               which don't have an app_id,
+%%                               point to this application.
+%%          default_endpoint_app If set to true,
+%%                               this parameter ensures that newly created
+%%                               endpoints, which don't have an app_id,
+%%                               point to this application.
+-spec create_application(Params::params()) -> json_term().
+create_application(Params) -> api(post, "Application/", Params).
+
+%% @spec delete_application(AppId::string()) -> json_term()
+%% @doc Deletes an application.
+-spec delete_application(AppId::string()) -> json_term().
+delete_application(AppId) -> api(delete, "Application/" ++ AppId ++ "/").
+
+%% @spec get_application(AppId::string()) -> json_term()
+%% @doc Grabs one specific application.
+-spec get_application(AppId::string()) -> json_term().
+get_application(AppId) -> api(get, "Application/" ++ AppId ++ "/").
+
+%% @spec get_applications() -> json_term()
+%% @doc Grabs all of the applications.
+-spec get_applications() -> json_term().
+get_applications() -> get_applications([]).
+
+%% @spec get_applications(Params::params()) -> json_term()
+%% @doc Grabs all of the applications.
+%%      Optional params:
+%%          subaccount Id or name or alias of the subaccount,
+%%                     in case only subaccount applications are needed.
+%%          limit      Used to display the number of results per page.
+%%                     The maximum number of results that can be fetched is 20.
+%%          offset     Denotes the number of value items by which
+%%                     the results should be offset.
+-spec get_applications(Params::params()) -> json_term().
+get_applications(Params) -> api(get, "Application/", Params).
+
+%% @spec modify_application(AppId::string(), Params::params()) -> json_term()
+%% @doc Modifies an existing application.
+%%      Optional params:
+%%          answer_url           The URL invoked by Plivo
+%%                               when a call executes this application.
+%%          answer_method        The method used to call the answer_url.
+%%                               Defaults to POST
+%%          hangup_url           The URL that is notified by Plivo
+%%                               when the call hangs up.
+%%          hangup_method        The method used to call the hangup_url.
+%%                               Defaults to POST
+%%          fallback_answer_url  Invoked by Plivo only if answer_url is
+%%                               unavailable or the XML response is invalid.
+%%                               Should contain a XML response.
+%%          fallback_method      The method used to call the
+%%                               fallback_answer_url.
+%%                               Defaults to POST.
+%%          message_url          The URL that is notified by Plivo
+%%                               when an inbound message is received.
+%%                               Defaults not set.
+%%          message_method       The method used to call the message_url.
+%%                               Defaults to POST.
+%%          default_number_app   If set to true,
+%%                               associates all newly created Plivo numbers
+%%                               that have not specified an app_id,
+%%                               to this application.
+%%          default_endpoint_app If set to true,
+%%                               associates all newly created Plivo endpoints
+%%                               that have not specified an app_id,
+%%                               to this application.
+-spec modify_application(AppId::string(), Params::params()) -> json_term().
+modify_application(AppId, Params) ->
+    api(post, "Application/" ++ AppId ++ "/", Params).
